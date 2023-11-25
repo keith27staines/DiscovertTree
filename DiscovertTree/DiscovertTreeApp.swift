01420c509816b76451bce7d7899cc11a009e83ed
keith27staines <keith27staines@icloud.com>
2023-11-25 19:02:48 +0000

    Initial Commit

---
+++ DiscovertTree/DiscovertTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscovertTreeApp.swift
+//  DiscovertTree
+//
+//  Created by Keith Staines on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiscovertTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
